afd0c93524250fde9c1df69380e4e36f6c1a634d
omairad <omair.ad.2000@gmail.com>
2022-05-29 17:28:34 +0300

    First commit

---
+++ Calculator.ViewModel/BasicCalculatorViewMode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.ViewModel
+{
+    public class BasicCalculatorViewMode : ViewModelBase
+    {
+        public BasicCalculatorViewMode()
+        {
+            Text ="ddd";
+        }
+
+
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                
+                OnPropertyChanged("Text");
+            }
+        }
+    }
+}
